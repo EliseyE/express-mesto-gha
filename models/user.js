3fcb1a9bb92f882937b3a5327a95ce5bdb38b9d4
Elisey Esaulkov <toelisey@yandex.ru>
2023-04-27 11:24:21 +0500

    fix: correct code style after lint

--- models/user.js
+++ models/user.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose');
 const { isURL } = require('../utils/utils');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Required field name'],
-    minlength: [2, 'Min length is 2 symbols. Current data is less than 2 symbols'],
-    maxlength: [30, 'Max length is 30 symbols. Current data is more than 30 symbols'],
-  },
-  about: {
-    type: String,
-    required: [true, 'Required field about'],
-    minlength: [2, 'Min length is 2 symbols. Current data is less than 2 symbols'],
-    maxlength: [30, 'Max length is 30 symbols. Current data is more than 30 symbols'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Required field avatar'],
-    validate: {
-      validator(v) { return isURL.test(v); },
-      message: 'Incorrect url format',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Required field name'],
+      minlength: [2, 'Min length is 2 symbols. Current data is less than 2 symbols'],
+      maxlength: [30, 'Max length is 30 symbols. Current data is more than 30 symbols'],
+    },
+    about: {
+      type: String,
+      required: [true, 'Required field about'],
+      minlength: [2, 'Min length is 2 symbols. Current data is less than 2 symbols'],
+      maxlength: [30, 'Max length is 30 symbols. Current data is more than 30 symbols'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Required field avatar'],
+      validate: {
+        validator(v) { return isURL.test(v); },
+        message: 'Incorrect url format',
+      },
     },
   },
-},
-{ versionKey: false },
+  { versionKey: false },
 );
 
 module.exports = mongoose.model('user', userSchema);
