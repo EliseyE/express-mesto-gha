b53bb5a0886de91089e4055b8a272d2597b0ffc8
Elisey Esaulkov <toelisey@yandex.ru>
2023-05-02 23:42:11 +0500

    fix: correct  central errors processing 14.001

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,22 +1,24 @@
 const Card = require('../models/card');
 const { errorHeandler } = require('../errors/errorHeandler');
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.json({ cards }))
-    .catch((err) => { errorHeandler(err, res); });
+    .catch((err) => { errorHeandler(err, res); })
+    .catch(next);
 };
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
     .then((card) => res.status(201).json({ card }))
-    .catch((err) => { errorHeandler(err, res); });
+    .catch((err) => { errorHeandler(err, res); })
+    .catch(next);
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   const { cardId } = req.params;
   const { userId } = req.user._id;
 
@@ -27,23 +29,26 @@ module.exports.deleteCard = (req, res) => {
       if (userId !== card.owner) return Promise.reject(new Error('Access denied'));
       res.json({ card });
     })
-    .catch((err) => { errorHeandler(err, res); });
+    .catch((err) => { errorHeandler(err, res); })
+    .catch(next);
 };
 
-module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } },
   { new: true },
 )
   .orFail()
   .then((card) => res.json({ card }))
-  .catch((err) => { errorHeandler(err, res); });
+  .catch((err) => { errorHeandler(err, res); })
+  .catch(next);
 
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
+module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } },
   { new: true },
 )
   .orFail()
   .then((card) => res.json({ card }))
-  .catch((err) => { errorHeandler(err, res); });
+  .catch((err) => { errorHeandler(err, res); })
+  .catch(next);
