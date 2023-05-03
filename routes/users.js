80da4ab562d3c87e6a0c29c8c9173e767c382488
Elisey Esaulkov <toelisey@yandex.ru>
2023-05-03 15:52:06 +0500

    fix: correct  router users: add hex() and required() for Joi in two usersRouter.patch('/me' and '/me/avatar'

--- routes/users.js
+++ routes/users.js
@@ -14,20 +14,20 @@ usersRouter.get('/me', getCurrentUserInfo);
 
 usersRouter.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required().length(24),
+    userId: Joi.string().hex().required().length(24),
   }),
 }), getUser);
 
 usersRouter.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 }), updateUserInfo);
 
 usersRouter.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(URL_REG_EXP),
+    avatar: Joi.string().required().pattern(URL_REG_EXP),
   }),
 }), updateUserAvatar);
 
