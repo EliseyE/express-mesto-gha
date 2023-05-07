f253e0b48167d5534a46b0a452226c7557e0eb94
Elisey Esaulkov <toelisey@yandex.ru>
2023-05-07 20:51:07 +0500

    feat: add logger and errorLogger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const routes = require('./routes');
 const centralErrorUnit = require('./errors/centralErrorUnit');
@@ -21,7 +22,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(requestLogger);
+
 app.use(routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(centralErrorUnit);
